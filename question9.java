7ef08808da7589086f767fb792df2c1076292ec5
lll <lvliangbupt@163.com>
2017-07-05 14:07:15 +0800

    leetcode 9

---
+++ question9.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package one;
+/**
+* @author lvliangliang E-mail:lvliangbupt@136.com
+* @version Create time：2017年7月4日 下午3:46:51
+* class illustration:
+*/
+/**
+ * @author NewUser
+ *
+ */
+public class question9 {
+	public static void main(String[] args) {
+		System.out.println(isPalindrome(12321));
+	}
+	public static boolean isPalindrome(int x) {
+        String s=Integer.toString(x);
+        StringBuffer sb=new StringBuffer(s);
+        String sbs=sb.reverse().toString();       
+        if(sbs.equals(s)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+
